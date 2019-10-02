802791b6a3d3c5e6ea66ef7092f0b98f1629cf57
benjamin <benjamin@benjamin-ThinkCentre-M900>
2019-10-02 12:03:43 -0400

    added more data

--- scripts/formation_vs_valence.py
+++ scripts/formation_vs_valence.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy.stats import linregress
-from data import fe, column, element, cohesive_energies, N2_engs, N2H_engs, d_band
+from data import fe, column, element, cohesive_energies, N2_engs, N2H_engs, d_band, electronegativity,d_cohesive, s_cohesive
 plt.rcParams["figure.figsize"] = (5.2,4.2)
+
+#cohesive_energies = s_cohesive
 
 n_col = []
 n_fe = []
@@ -22,6 +24,8 @@ for i, e in enumerate(fe):
 n_ele_N2 = []
 for e, n2,n2h in zip(element,N2_engs,N2H_engs):
     if n2 is None:
+        continue
+    if e in ['Fe', 'Ni', 'Co', 'Mn', 'Cr']:
         continue
     n_N2_engs.append(n2)
     nn_N2H_engs.append(n2h)
@@ -45,20 +49,40 @@ plt.show()
 
 ############################ cohesive energy N2H
 
-slope, intercept, r_value, p_value, std_err = linregress([cohesive_energies[a] for a in n_ele], n_N2H_engs)
+slope, intercept, r_value, p_value, std_err = linregress([cohesive_energies[a] for a in n_ele_N2],
+                                                         nn_N2H_engs)
 
 plt.title('Metal Cohesive Energy vs N$_2$H Binding Energy')
 plt.ylabel('N$_2$H Binding Energy')
 plt.xlabel('Metal Cohesive Energy (eV/atom)')
-co_e = [cohesive_energies[a] for a in n_ele]
-plt.text(max(co_e)-0.75,max( n_N2H_engs), 'R$^2$ = {}'.format(round(r_value ** 2,2)))
-plt.scatter(co_e, n_N2H_engs)
+co_e = [cohesive_energies[a] for a in n_ele_N2]
+plt.text(max(co_e)-0.75,max(nn_N2H_engs), 'R$^2$ = {}'.format(round(r_value ** 2,2)))
+plt.scatter(co_e, nn_N2H_engs)
 plt.plot([min(co_e),max(co_e)],np.array([min(co_e),max(co_e)]) * slope + intercept)
-for c, e, t in zip(co_e, n_N2H_engs, n_ele):
+for c, e, t in zip(co_e, nn_N2H_engs, n_ele_N2):
     plt.text(c + 0.05, e + 0.05, t)
 
 plt.savefig('cohesive_eng_vs_N2H.pdf')
 plt.show()
+
+
+################################ cohesive energy N2
+slope, intercept, r_value, p_value, std_err = linregress([cohesive_energies[a] for a in n_ele_N2],
+                                                         n_N2_engs)
+
+plt.title('Metal Cohesive Energy vs N$_2$ Binding Energy')
+plt.ylabel('N$_2$ Binding Energy')
+plt.xlabel('Metal Cohesive Energy (eV/atom)')
+co_e = [cohesive_energies[a] for a in n_ele_N2]
+plt.text(max(co_e)-0.75,max(n_N2_engs), 'R$^2$ = {}'.format(round(r_value ** 2,2)))
+plt.scatter(co_e, n_N2_engs)
+plt.plot([min(co_e),max(co_e)],np.array([min(co_e),max(co_e)]) * slope + intercept)
+for c, e, t in zip(co_e, n_N2_engs, n_ele_N2):
+    plt.text(c + 0.05, e + 0.05, t)
+
+plt.savefig('cohesive_eng_vs_N2.pdf')
+plt.show()
+
 
 ########################### N2 vs N2H
 
@@ -70,7 +94,7 @@ plt.xlabel('N$_2$ Binding Energy (eV)')
 plt.text(max(n_N2_engs)-0.75,max(nn_N2H_engs), 'R$^2$ = {}'.format(round(r_value ** 2,2)))
 plt.scatter(n_N2_engs, nn_N2H_engs)
 plt.plot([min(n_N2_engs),max(n_N2_engs)],np.array([min(n_N2_engs),max(n_N2_engs,)]) * slope + intercept)
-for c, e, t in zip(n_N2_engs, nn_N2H_engs, n_ele):
+for c, e, t in zip(n_N2_engs, nn_N2H_engs, n_ele_N2):
     plt.text(c + 0.05, e + 0.05, t)
 
 plt.savefig('N2_vs_N2H.pdf')
@@ -98,6 +122,111 @@ plt.savefig('d_band_vs_formation.pdf')
 plt.show()
 
 ########################### row bar charts
+
+
+plt.rcParams["figure.figsize"] = (5.5,8)
+
+fig, _axs = plt.subplots(nrows=2, ncols=1)
+axs = _axs.flatten()
+
+row_1_elements = ['Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu']
+row_2_elements = ['Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag']
+row_3_elements = ['', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au']
+
+positive_row_energies = [[],[],[]]
+negative_row_energies = [[],[],[]]
+
+rows = [row_1_elements, row_2_elements, row_3_elements]
+
+for i, row in enumerate(rows):
+    for element_sym in row:
+        if element_sym == '':
+            eng = 0
+        elif element_sym not in element:
+            eng = 0
+        else:
+            index = element.index(element_sym)
+            eng = N2_engs[index] 
+            if eng is None:
+                eng = 0
+        if eng >= 0:
+            positive_row_energies[i].append(eng)
+            negative_row_energies[i].append(0)
+        elif eng < 0:
+            positive_row_energies[i].append(0)
+            negative_row_energies[i].append(eng)
+del rows[0]
+del positive_row_energies[0]
+del negative_row_energies[0]
+
+for i, row in enumerate(rows):
+    axs[i].bar(row, positive_row_energies[i], color='#0390fc')
+    axs[i].bar(row, negative_row_energies[i], color='#0390fc')
+    #axs[i].set_title('Row {}'.format(i + 4))
+    axs[i].set_ylim([-1.6,0])
+    if i == 1:
+        axs[i]. set_ylabel('N$_2$ Adsorption Energy (eV)')
+    if i == 1:
+        axs[i].set_xlabel('Element')
+    if i == 0:
+        axs[i].set_title('N$_2$ Adsorption Energy by Periodic Row')
+plt.savefig('N2_adsorption_rows.pdf')
+plt.show()
+#####################
+
+plt.rcParams["figure.figsize"] = (5.5,8)
+
+fig, _axs = plt.subplots(nrows=2, ncols=1)
+axs = _axs.flatten()
+
+row_1_elements = ['Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu']
+row_2_elements = ['Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag']
+row_3_elements = ['', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au']
+
+positive_row_energies = [[],[],[]]
+negative_row_energies = [[],[],[]]
+
+rows = [row_1_elements, row_2_elements, row_3_elements]
+
+for i, row in enumerate(rows):
+    for element_sym in row:
+        if element_sym == '':
+            eng = 0
+        elif element_sym not in element:
+            eng = 0
+        else:
+            index = element.index(element_sym)
+            eng = N2H_engs[index]
+            if eng is None:
+                eng = 0
+        if eng >= 0:
+            positive_row_energies[i].append(eng)
+            negative_row_energies[i].append(0)
+        elif eng < 0:
+            positive_row_energies[i].append(0)
+            negative_row_energies[i].append(eng)
+del rows[0]
+del positive_row_energies[0]
+del negative_row_energies[0]
+
+for i, row in enumerate(rows):
+    axs[i].bar(row, positive_row_energies[i], color='#0390fc')
+    axs[i].bar(row, negative_row_energies[i], color='#0390fc')
+    #axs[i].set_title('Row {}'.format(i + 4))
+    axs[i].set_ylim([-1.5,2])
+    if i == 1:
+        axs[i]. set_ylabel('N$_2$H Adsorption Energy (eV)')
+    if i == 1:
+        axs[i].set_xlabel('Element')
+    if i == 0:
+        axs[i].set_title('N$_2$H Adsorption Energy by Periodic Row')
+plt.savefig('N2H_adsorption_rows.pdf')
+plt.show()
+
+
+####################################
+
+"""
 plt.rcParams["figure.figsize"] = (5.5,8)
 
 fig, _axs = plt.subplots(nrows=3, ncols=1)
@@ -191,3 +320,4 @@ for i, row in enumerate(rows):
 
 plt.savefig('N2H_adsorption_rows')
 plt.show()
+"""
